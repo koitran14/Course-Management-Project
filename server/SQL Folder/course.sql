1a894d7a04a87d568eb71829fc7ea0596fe37686
Trần Ngọc Đăng Khôi <khoitranngocdg@gmail.com>
2023-12-01 20:05:08 +0700

    update foreign key and attributes

--- server/SQL Folder/course.sql
+++ server/SQL Folder/course.sql
@@ -28,22 +28,17 @@ CREATE TABLE Student (
     StudentID VARCHAR(25) primary key,
     UserID VARCHAR(25),
     DeptID VARCHAR(25),
-    EnrollID VARCHAR(25)
 )
 
 CREATE TABLE Department(
     DeptID varchar(25) primary key,
     DeptName varchar(50),
-    TutorID varchar(25),
-    StudentID varchar(25),
-    CourseID varchar(25)
 )
 
 CREATE TABLE Tutor (
     TutorID VARCHAR(25) PRIMARY KEY,
     UserID VARCHAR(25),
     DeptID VARCHAR(25),
-    CourseID varchar(25)
 )
 
 CREATE TABLE Enroll (
@@ -56,11 +51,7 @@ CREATE TABLE Enroll (
 CREATE TABLE Course (
     CourseID VARCHAR(25) PRIMARY KEY,
     CourseName VARCHAR(50),
-    AnID VARCHAR(25),
-    ConID VARCHAR(25),
-    A_ID VARCHAR(25),
-    EnrollID VARCHAR(25),
-    TutorID VARCHAR(25)
+    TutorID VARCHAR(25),
 )
 
 CREATE TABLE Content (
@@ -68,7 +59,7 @@ CREATE TABLE Content (
     ConTitle VARCHAR (50),
     ConDesc VARCHAR (150),
     ConDate VARCHAR (200),
-    ConAttachID VARCHAR(25),
+    CourseID VARCHAR(25),
 )
 
 CREATE TABLE Announcement(
@@ -76,7 +67,7 @@ CREATE TABLE Announcement(
     AnTitle VARCHAR(50),
     AnDesc VARCHAR(150),
     AnDate DATE,
-    AnAttachID VARCHAR(25),
+    CourseID VARCHAR(25),
 )
 
 CREATE TABLE Assignment (
@@ -87,7 +78,7 @@ CREATE TABLE Assignment (
     A_DueDate DATE,
     A_TextSubmission VARCHAR(500),
     A_Comment VARCHAR(150),
-    A_AttachID VARCHAR(25)
+    CourseID VARCHAR(25),
 )
 
 CREATE TABLE Attachment (
@@ -95,89 +86,59 @@ CREATE TABLE Attachment (
     Attach_FileName VARCHAR(50),
     Attach_FileType VARCHAR(10),
     Attach_Size FLOAT,
-    Attach_Date DATE
+    Attach_Date DATE,
+    CourseID VARCHAR(25),
+    A_ID VARCHAR(25),
+    AnID VARCHAR(25),
+    ConID VARCHAR(25),
 )
 
-
--- foreign key
+-- FOREIGN KEY
 ALTER TABLE [User]
-ADD CONSTRAINT FK_User_Login
-FOREIGN KEY (LoginID) REFERENCES Login(LoginID);
+ADD FOREIGN KEY (LoginID) REFERENCES Login(LoginID);
 
 ALTER TABLE [User]
-ADD CONSTRAINT FK_User_Role
-FOREIGN KEY (RoleID) REFERENCES Role(RoleID);
+ADD FOREIGN KEY (RoleID) REFERENCES Role(RoleID);
+
+ALTER TABLE Student
+ADD FOREIGN KEY (UserID) REFERENCES [User](UserID);
+
+ALTER TABLE Student
+ADD FOREIGN KEY (DeptID) REFERENCES Department(DeptID);
+
+ALTER TABLE Tutor
+ADD FOREIGN KEY (UserID) REFERENCES [User](UserID);
+
+ALTER TABLE Tutor
+ADD FOREIGN KEY (DeptID) REFERENCES Department(DeptID);
 
 ALTER TABLE Enroll
-ADD CONSTRAINT FK_Enroll_Student
-FOREIGN KEY (StudentID) REFERENCES Student(StudentID);
+ADD FOREIGN KEY (StudentID) REFERENCES Student(StudentID);
 
 ALTER TABLE Enroll
-ADD CONSTRAINT FK_Enroll_Course
-FOREIGN KEY (CourseID) REFERENCES Course(CourseID);
-
-ALTER TABLE Student
-ADD CONSTRAINT FK_Student_User
-FOREIGN KEY (UserID) REFERENCES [User](UserID);
-
-ALTER TABLE Student
-ADD CONSTRAINT FK_Student_Department
-FOREIGN KEY (DeptID) REFERENCES Department(DeptID);
-
-ALTER TABLE Student
-ADD CONSTRAINT FK_Student_Enroll
-FOREIGN KEY (EnrollID) REFERENCES Enroll(EnrollID);
-
-ALTER TABLE Department
-ADD CONSTRAINT FK_Department_Tutor
-FOREIGN KEY (TutorID) REFERENCES Tutor(TutorID);
-
-ALTER TABLE Department
-ADD CONSTRAINT FK_Department_Student
-FOREIGN KEY (StudentID) REFERENCES Student(StudentID);
-
-ALTER TABLE Department
-ADD CONSTRAINT FK_Department_Course
-FOREIGN KEY (CourseID) REFERENCES Course(CourseID);
-
-ALTER TABLE Tutor
-ADD CONSTRAINT FK_Tutor_User
-FOREIGN KEY (UserID) REFERENCES [User](UserID);
-
-ALTER TABLE Tutor
-ADD CONSTRAINT FK_Tutor_Department
-FOREIGN KEY (DeptID) REFERENCES Department(DeptID);
-
-ALTER TABLE Tutor
-ADD CONSTRAINT FK_Tutor_Course
-FOREIGN KEY (CourseID) REFERENCES Course(CourseID);
+ADD FOREIGN KEY (CourseID) REFERENCES Course(CourseID);
 
 ALTER TABLE Course
-ADD CONSTRAINT FK_Course_Tutor
-FOREIGN KEY (TutorID) REFERENCES Tutor(TutorID);
-
-ALTER TABLE Course
-ADD CONSTRAINT FK_Course_Annnouncement
-FOREIGN KEY (AnID) REFERENCES Announcement(AnID);
-
-ALTER TABLE Course
-ADD CONSTRAINT FK_Course_Content
-FOREIGN KEY (ConID) REFERENCES Content(ConID);
-
-ALTER TABLE Course
-ADD CONSTRAINT FK_Course_Assignment
-FOREIGN KEY (A_ID) REFERENCES Assignment(A_ID);
-
-ALTER TABLE Course
-ADD CONSTRAINT FK_Course_Enroll
-FOREIGN KEY (EnrollID) REFERENCES Enroll(EnrollID);
-
-
-ALTER TABLE Announcement
-ADD CONSTRAINT FK_Announcement_Attachment
-FOREIGN KEY (AnAttachID) REFERENCES Attachment(AttachID);
+ADD FOREIGN KEY (TutorID) REFERENCES Tutor(TutorID);
 
 ALTER TABLE Content
-ADD CONSTRAINT FK_Content_Attachment
-FOREIGN KEY (ConAttachID) REFERENCES Attachment(AttachID);
+ADD FOREIGN KEY (CourseID) REFERENCES Course(CourseID);
+
+ALTER TABLE Announcement
+ADD FOREIGN KEY (CourseID) REFERENCES Course(CourseID);
+
+ALTER TABLE Assignment
+ADD FOREIGN KEY (CourseID) REFERENCES Course(CourseID);
+
+ALTER TABLE Attachment
+ADD FOREIGN KEY (CourseID) REFERENCES Course(CourseID);
+
+ALTER TABLE Attachment
+ADD FOREIGN KEY (A_ID) REFERENCES Assignment(A_ID);
+
+ALTER TABLE Attachment
+ADD FOREIGN KEY (AnID) REFERENCES Announcement(AnID);
+
+ALTER TABLE Attachment
+ADD FOREIGN KEY (ConID) REFERENCES Content(ConID);
 
--- server/SQL Folder/fakeData.sql
+++ server/SQL Folder/fakeData.sql
@@ -1,22 +1,90 @@
-USE CourseDB;
-
--- Inserting data into the Role table
+--   Role table
 INSERT INTO Role (RoleID, RoleName, RoleDescription)
-VALUES 
-    ('R1', 'Admin', 'Administrator role'),
-    ('R2', 'Manager', 'Managerial role'),
-    ('R3', 'User', 'Regular user role');
+VALUES
+    ('R001', 'Admin', 'Administrative Role'),
+    ('R002', 'Tutor', 'Teaching Role'),
+    ('R003', 'Student', 'Learning Role');
 
--- Inserting data into the Login table
+--   Login table
 INSERT INTO Login (LoginID, LoginUserName, LoginPassword)
-VALUES 
-    ('JD123', 'john_doe', 'password123'),
-    ('AS456', 'alice_smith', 'pass456'),
-    ('MJ789', 'mark_johnson', 'secret123');
+VALUES
+    ('L001', 'admin_user', 'admin_pass'),
+    ('L002', 'tutor_user', 'tutor_pass'),
+    ('L003', 'student_user', 'student_pass');
 
--- Inserting data into the [User] table
+--   [User] table
 INSERT INTO [User] (UserID, UserLastName, UserFirstName, UserEmail, UserDOB, LoginID, RoleID)
-VALUES 
-    ('U1', 'Doe', 'John', 'john@example.com', '1990-05-15', 'JD123', 'R1'),
-    ('U2', 'Smith', 'Alice', 'alice@example.com', '1985-09-20', 'AS456', 'R2'),
-    ('U3', 'Johnson', 'Mark', 'mark@example.com', '1995-02-10', 'MJ789', 'R3');
+VALUES
+    ('U001', 'Admin', 'Alice', 'admin@example.com', '1990-01-01', 'L001', 'R001'),
+    ('U002', 'Tutor', 'Bob', 'tutor@example.com', '1985-05-15', 'L002', 'R002'),
+    ('U003', 'Student', 'Charlie', 'student@example.com', '2000-09-20', 'L003', 'R003');
+
+--   Department table
+INSERT INTO Department (DeptID, DeptName)
+VALUES
+    ('D001', 'Computer Science'),
+    ('D002', 'Mathematics'),
+    ('D003', 'Physics');
+
+--   Student table
+INSERT INTO Student (StudentID, UserID, DeptID)
+VALUES
+    ('S001', 'U003', 'D001'),
+    ('S002', 'U003', 'D002'),
+    ('S003', 'U003', 'D003');
+
+--   Tutor table
+INSERT INTO Tutor (TutorID, UserID, DeptID)
+VALUES
+    ('T001', 'U002', 'D001'),
+    ('T002', 'U002', 'D002'),
+    ('T003', 'U002', 'D003');
+
+--   Course table
+INSERT INTO Course (CourseID, CourseName, TutorID)
+VALUES
+    ('C001', 'Introduction to Programming', 'T001'),
+    ('C002', 'Calculus I', 'T002'),
+    ('C003', 'Mechanics', 'T003');
+
+--   Enroll table
+INSERT INTO Enroll (EnrollID, EnrollDate, StudentID, CourseID)
+VALUES
+    ('E001', '2023-01-01', 'S001', 'C001'),
+    ('E002', '2023-01-01', 'S002', 'C002'),
+    ('E003', '2023-01-01', 'S003', 'C003');
+
+--   Content table
+INSERT INTO Content (ConID, ConTitle, ConDesc, ConDate, CourseID)
+VALUES
+    ('CN001', 'Introduction to Variables', 'Basic concepts of variables', '2023-01-05', 'C001'),
+    ('CN002', 'Limits and Derivatives', 'Understanding limits and derivatives', '2023-01-10', 'C002'),
+    ('CN003', 'Newtonian Mechanics', 'Introductory mechanics principles', '2023-01-15', 'C003');
+
+--   Announcement table
+INSERT INTO Announcement (AnID, AnTitle, AnDesc, AnDate, CourseID)
+VALUES
+    ('A001', 'Important Notice', 'Exam schedule changed', '2023-02-01', 'C001'),
+    ('A002', 'Assignment Reminder', 'Submit your assignment by next week', '2023-02-05', 'C002'),
+    ('A003', 'Guest Lecture', 'Special lecture by visiting professor', '2023-02-10', 'C003');
+
+--   Assignment table
+INSERT INTO Assignment (A_ID, A_Title, A_Desc, A_StartAt, A_DueDate, A_TextSubmission, A_Comment, CourseID)
+VALUES
+    ('AS001', 'Variables Exercise', 'Practice on variables', '2023-01-07', '2023-01-14', 'Text submission here', 'Remember to include explanations', 'C001'),
+    ('AS002', 'Derivatives Problems', 'Solving derivative exercises', '2023-01-12', '2023-01-19', 'Text submission here', 'Please show detailed steps', 'C002'),
+    ('AS003', 'Mechanics Quiz', 'Short quiz on mechanics concepts', '2023-01-18', '2023-01-25', 'Text submission here', 'Review your notes before attempting', 'C003');
+
+--   Attachment table 
+INSERT INTO Attachment (AttachID, Attach_FileName, Attach_FileType, Attach_Size, Attach_Date, CourseID, A_ID, AnID, ConID)
+VALUES
+    ('AT001', 'variables.pdf', 'pdf', 123.45, '2023-01-14', 'C001', 'AS001', NULL, 'CN001'),
+    ('AT002', 'derivatives.docx', 'docx', 234.56, '2023-01-19', 'C002', 'AS002', NULL, 'CN002'),
+    ('AT003', 'mechanics_notes.pdf', 'pdf', 345.67, '2023-01-25', 'C003', NULL, NULL, 'CN003'),
+    ('AT004', 'assignment_1.pdf', 'pdf', 456.78, '2023-02-01', 'C001', 'AS001', NULL, NULL),
+    ('AT005', 'calculus_homework.docx', 'docx', 567.89, '2023-02-05', 'C002', 'AS002', NULL, NULL),
+    ('AT006', 'momentum_equations.pdf', 'pdf', 678.90, '2023-02-10', 'C003', NULL, 'A003', 'CN003'),
+    ('AT007', 'variables_solution.pdf', 'pdf', 789.01, '2023-02-15', 'C001', 'AS001', NULL, NULL),
+    ('AT008', 'calculus_exam.pdf', 'pdf', 890.12, '2023-02-20', 'C002', 'AS002', 'A002', NULL),
+    ('AT009', 'forces_lab_report.docx', 'docx', 901.23, '2023-02-25', 'C003', NULL, 'A003', NULL),
+    ('AT010', 'programming_assignment.py', 'py', 1023.45, '2023-03-01', 'C001', 'AS001', NULL, NULL);
